3b1faf32a55126e4458d472bb19ddd2cdd75509e
Nalvazhuthi <nalvazhuthi@hexrfactory.com>
2025-05-20 16:49:47 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./style/main.scss";
+import Nav from "./components/nav";
+
+const App = () => {
+  return (
+    <div className="content-container">
+      <Nav />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/nav.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <div className="nav-container">
+      <div className="brand-name">Zentry Studio</div>
+      <div className="navigation-wrapper">
+        <div className="navigation">About</div>
+        <div className="navigation">Services</div>
+        <div className="navigation">Projects</div>
+        <div className="navigation">Contact Us</div>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
